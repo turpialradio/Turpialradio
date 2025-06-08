d7fad505be437fa0e32f48dbe675abfe68afd875
turpialradio <radiotvturpial@gmail.com>
2025-06-07 22:37:58 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ document.querySelector("h1").addEventListener("click", () => {
 });
 
 function toggleMenu() {
-    var menuContainer = document.querySelector(".menu-container");
-
-    if (menuContainer.style.left === "-250px") {
-        menuContainer.style.left = "0px";
-    } else {
-        menuContainer.style.left = "-250px";
-    }
+  var menuContainer = document.querySelector(".menu-container");
+  menuContainer.classList.toggle("active");
 }
+
 
